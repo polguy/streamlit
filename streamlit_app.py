334b9495bc0a5186a1dedabcaad55e93fbe42ca7
polguy <jacques.augizeau@gmail.com>
2023-03-12 12:24:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,3 @@
-
-import streamlit as st
-import pandas as pd
-
-st.write("""
-# My first app
-Hello *world!*
-""")
-
-df = pd.read_csv("my_data.csv")
-st.line_chart(df)
+!pip install openai
+!pip install streamlit
+!pip install python-dotenv
