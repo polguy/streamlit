1fec6a670071785e7e96c9202e37d1db3b9bac66
polguy <jacques.augizeau@gmail.com>
2023-03-12 12:31:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
 st.write("""
-# My first app
+# My first app with Streamlit
+## https://lablab.ai/t/gpt-trip-scheduler
+## https://github.com/polguy/streamlit/edit/main/streamlit_app.py
 HELLO WORLD*
 """)
 
